31ecc66d9e760e62c487889fde54bd035fbba634
Alaa Fayed <alaafayed810@gmail.com>
2023-11-01 04:55:20 +0200

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 for n in range(0, 10, 1):
     for n2 in range(1, 10, 1):
-        if n2 > n and n != 8:
-            print("{}{}".format(n, n2), end=", ")
+        if n >= n2:
+            continue
         elif n == 8 and n2 == 9:
             print("{}{}".format(n, n2))
-        break
+        else:
+            print("{}{}".format(n, n2), end=", ")
