45aa318c6cc4da31d4dfcd1c474d83ec654a47ed
Alaa Fayed <alaafayed810@gmail.com>
2023-11-02 00:24:55 +0200

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in range(len(str)):
+    for c in str:
         if ord(c) >= 65 and ord(c) <= 90:
             print("{}".format(c), end="")
         print()
