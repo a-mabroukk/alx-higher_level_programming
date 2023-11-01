589cb6aa91814b4161f8a5f2df4733ecea8db796
Alaa Fayed <alaafayed810@gmail.com>
2023-11-01 17:09:37 +0200

    7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+f_l = word[:3]
+l_l = word[7:]
+m_w = word[0:9]
+print(f"First 3 letters: {f_l}")
+print(f"Last 2 letters: {l_l}")
+print(f"Middle word: {m_w}")
