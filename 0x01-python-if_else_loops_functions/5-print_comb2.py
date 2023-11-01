013777ebe52fef57b10da86d620776c15c6c3607
Alaa Fayed <alaafayed810@gmail.com>
2023-11-01 03:48:13 +0200

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100, 1):
     if n < 99:
-        print("{:2d}".format(n), end=", ")
+        print("{:02d}".format(n), end=", ")
     else:
-        print("n")
+        print(n)
