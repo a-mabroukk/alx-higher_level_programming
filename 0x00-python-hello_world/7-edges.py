591cdf2941ae713c3c9651d773abda2ede43ec50
Alaa Fayed <alaafayed810@gmail.com>
2023-11-01 17:37:02 +0200

    7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 f_l = word[:3]
 l_l = word[7:]
-m_w = word[1:8]
+m_w = word[1:-1]
 print(f"First 3 letters: {f_l}")
 print(f"Last 2 letters: {l_l}")
 print(f"Middle word: {m_w}")
