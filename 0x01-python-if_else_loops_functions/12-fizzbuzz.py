5df14d9b27e0b745469dd3d7b77a2c7b1f4fcdc7
Alaa Fayed <alaafayed810@gmail.com>
2023-11-02 02:34:36 +0200

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 5 == 0 and n % 3 == 0:
+            print("{}".format("FizzBuzz"), end= " ")
+            continue
+        if n % 3 == 0:
+            print("{}".format("Fizz"), end= " ")
+        elif n % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(n), end=" ")
