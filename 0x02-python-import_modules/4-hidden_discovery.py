c03e1c91aed0c277d487bbb09e7fc824cb9f7712
Alaa Fayed <alaafayed810@gmail.com>
2023-11-03 03:16:49 +0200

    5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    name = dir(hidden_4)
-    for name in range(len(name)):
-        if name[i][0] != '-' and name[i][1] != '-':
-            print(name[i])
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
