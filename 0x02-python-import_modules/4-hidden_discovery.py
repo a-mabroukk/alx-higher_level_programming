ed5667b20371ab934a4a3bddcea211b21d2d58a3
Alaa Fayed <alaafayed810@gmail.com>
2023-11-03 02:33:16 +0200

    4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    name = dir(hidden_4)
+    for name in range(len(name)):
+        if name[i][0] != '-' and name[i][1] != '-':
+            print(name[i])
