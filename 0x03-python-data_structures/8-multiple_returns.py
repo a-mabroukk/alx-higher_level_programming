b23bba32b5739777c88884c3799c2b6a0767c2d7
Alaa Fayed <alaafayed810@gmail.com>
2023-11-07 17:31:39 +0200

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,5 @@
 def multiple_returns(sentence):
     if sentence == ():
         sentence[0] = None
-    return len(sentence), sentence[0]
+        return (0, None)
+    return (len(sentence), sentence[0])
