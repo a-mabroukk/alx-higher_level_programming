d4a124472aa5b4fe8ebeae7ddb82b1e0bd740735
Alaa Fayed <alaafayed810@gmail.com>
2023-11-10 23:57:04 +0200

    100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,6 +7,6 @@ def weight_average(my_list=[]):
         return 0
     for int_num in my_list:
         average += int_num[0] * int_num[1]
-        devi +=  int_num[1]
+        devi += int_num[1]
         floated = float(average / devi)
     return floated
