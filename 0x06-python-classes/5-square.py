eb0693fc3a9edb44443e8a9de013d7f2104729b5
Alaa Fayed <alaafayed810@gmail.com>
2023-11-23 00:13:56 +0200

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""class Square definition."""
+
+
+class Square:
+    """a class defines crucial for a square by: Private instance attribute
+
+    Attributes:
+        size: size of square
+    """
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        self.__size = value
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def __init__(self, size=0):
+        """Creates new instances of square."""
+
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Public instance method print in stdout square with character #"""
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
