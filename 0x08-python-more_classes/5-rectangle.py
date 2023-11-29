9597a43e27b096ab20513306da29128f19f1fe7a
Alaa Fayed <alaafayed810@gmail.com>
2023-11-29 22:25:41 +0200

    5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
     def __repr__(self):
         """Returns a string representation of the rectangle"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         """Deletes an instance of a class"""
         print("{:s}".format("Bye rectangle..."))
