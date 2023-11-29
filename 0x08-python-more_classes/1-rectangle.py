33cd5afb6a2b61dc99879e8873b8c5f47c1131dc
Alaa Fayed <alaafayed810@gmail.com>
2023-11-29 19:12:32 +0200

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""definition class"""
+
+
+class Rectangle:
+    """defines a rectangle by: Private instance attribute
+    Attribute:
+    width: width of rectangle
+    """
+    @property
+    def width(self):
+        """property to retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property setter to set it"""
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """property to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """property setter to set it"""
+        self.__height = value
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def __init__(self, width=0, height=0):
+        """Instantiation"""
+        self.__width = width
+        self.__height = height
