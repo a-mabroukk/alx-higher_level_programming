0ec27c4211c4c03abacafd64c991cc74a95c7eee
Alaa Fayed <alaafayed810@gmail.com>
2023-12-05 00:13:51 +0200

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class definition"""
+
+
+class BaseGeometry:
+    """Public instance method"""
+    def area(self):
+        """ raises an Exception with message"""
+        raise Exception("area() is not implemented")
