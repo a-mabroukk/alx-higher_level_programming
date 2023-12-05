3b98472496f912138940280bab97d48ef3e9615e
Alaa Fayed <alaafayed810@gmail.com>
2023-12-05 19:55:46 +0200

    7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import JSON module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """a script that adds all arguments to a Python list, and
+    then save them to a file
+    """
+    with open(filename, 'w', encoding='UTF-8') as item:
+        add_item = json.dump(my_obj, item)
