ec83f383c5bf66cae070711c7122d5dfecffdb71
Alaa Fayed <alaafayed810@gmail.com>
2023-12-05 20:53:54 +0200

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """import JSON module"""
-import json
+import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """a script that adds all arguments to a Python list, and
-    then save them to a file
-    """
-    with open(filename, 'w', encoding='UTF-8') as item:
-        json.dump(my_obj, item)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
+
+argc = len(sys.argv)
+
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
+
+save_to_json_file(lst, "add_item.json")
