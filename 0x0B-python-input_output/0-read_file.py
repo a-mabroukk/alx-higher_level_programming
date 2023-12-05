f3390cb51f8ee6f2188b7ab84b24d7378e387ed5
Alaa Fayed <alaafayed810@gmail.com>
2023-12-05 14:50:47 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""function that reads a text file"""
+
+
+
 def read_file(filename=""):
+    """reads a text file"""
     with open('my_file_0.txt', 'r', encoding='UTF-8') as f:
         r_file = f.read()
         print(r_file)
