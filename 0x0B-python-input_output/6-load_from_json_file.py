2d714975d9304be68e6479a660178672ce8f8d5f
Alaa Fayed <alaafayed810@gmail.com>
2023-12-05 18:13:43 +0200

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""import JSON module"""
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object from a “JSON file"""
+    with open(filename, 'r') as json_file:
+        Create_JSON_file = json.load(json_file)
