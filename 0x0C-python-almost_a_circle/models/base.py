2590780b2ded0d9808c90ab2c24726d076b012e7
Alaa Fayed <alaafayed810@gmail.com>
2023-12-11 20:02:25 +0200

    all

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or list_dictionaries == [{}]:
-            return "[]"
+            return list_dictionaries is "[]"
         else:
             return json.dumps(list_dictionaries)
 
