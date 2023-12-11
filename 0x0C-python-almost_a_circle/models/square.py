8030662f8266f535a3072537c0e27aee23e6255a
Alaa Fayed <alaafayed810@gmail.com>
2023-12-11 23:14:14 +0200

    all

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,11 +12,10 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """return [Square] (<id>) <x>/<y> - <size> - in our case,
+        """return , [Square] (<id>) <x>/<y> - <size> - in our case,
         width or height
         """
-        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
-            type(self).__name__, self.id, self.x, self.y, self.width)
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
