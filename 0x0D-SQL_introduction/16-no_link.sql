5d8f655371b1ced07bc938ea0c893b5df378bcb0
Alaa Fayed <alaafayed810@gmail.com>
2024-02-15 00:46:51 +0200

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of table of the database in my MySQL server
-SELECT score, name FROM second_table WHERE name = ""
+SELECT score, name FROM second_table WHERE name != ""
 ORDER BY score DESC;
