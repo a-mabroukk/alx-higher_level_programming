89d3b7b3a5c5dcaa397509cf838377b2843d73f1
Alaa Fayed <alaafayed810@gmail.com>
2024-02-14 23:14:47 +0200

    11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records in the table second_table of the database
-SELECT score, name FROM second_table  WHERE score BETWEEN 10 AND 14
+SELECT score, name FROM second_table WHERE score BETWEEN 10 AND 14
 ORDER BY score DESC;
