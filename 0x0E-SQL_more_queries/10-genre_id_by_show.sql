83e47f05dafcb1028db5ecbbb9033b0092245021
Alaa Fayed <alaafayed810@gmail.com>
2024-02-16 01:28:20 +0200

    10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+--  script that lists all shows contained that have at least one genre linked
+SELECT s.title, g.genre_id FROM tv_shows s INNER JOIN tv_show_genres g
+ON s.id = g.show_id 
+ORDER BY s.title, g.genre_id ASC;
