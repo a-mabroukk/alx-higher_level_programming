054eaf84289290c7488bd6cd3a414514cbc6773f
Alaa Fayed <alaafayed810@gmail.com>
2024-02-16 03:02:40 +0200

    16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows, and all genres linked to that show, from the database
+SELECT s.title, g.name
+FROM (tv_shows s LEFT JOIN tv_show_genres sg ON s.id = sg.show_id)
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+ORDER BY s.title, g.name ASC;
