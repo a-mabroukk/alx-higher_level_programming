304295612c6dec81cd8efb209baf2507a76ec479
Alaa Fayed <alaafayed810@gmail.com>
2024-02-16 02:57:38 +0200

    14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- script that uses the database to lists all genres of the show
+SELECT g.name
+FROM (tv_shows s JOIN tv_show_genres sg ON s.id = sg.show_id)
+JOIN tv_genres g ON sg.genre_id = g.id
+WHERE s.title = "Dexter" ORDER BY g.name ASC;
