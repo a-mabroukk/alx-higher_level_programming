648ca56ba06ac58b6fd43732f835125154ac76ba
Alaa Fayed <alaafayed810@gmail.com>
2024-03-12 02:45:47 +0200

    1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// a script that prints 3 lines//
+const firline = 'C is fun';
+const secline = 'Python is cool';
+const thline = 'JavaScript is amazing';
+
+console.log(firline);
+console.log(secline);
+console.log(thline);
