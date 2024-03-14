a7619cc260370685860516dc5334236aa8e78cce
Alaa Fayed <alaafayed810@gmail.com>
2024-03-14 03:11:47 +0200

    6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    super.print(c);
+  }
+};
