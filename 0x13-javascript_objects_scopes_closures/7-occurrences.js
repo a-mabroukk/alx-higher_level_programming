a6e4771d851e873a85c1902fc946322e7005ab5b
Alaa Fayed <alaafayed810@gmail.com>
2024-03-14 03:35:35 +0200

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  
+
   for (let i = 0; i < list.length; i++) {
     count = (list[i] === searchElement ? count + 1 : count);
   }
