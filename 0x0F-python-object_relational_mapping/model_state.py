5da40ff64dcaac030a558f8cec2970488c66584f
Alaa Fayed <alaafayed810@gmail.com>
2024-03-19 01:29:04 +0200

    6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,8 +2,10 @@
 import MySQLAlchemy
 from MySQLAlchemy import Column, Integer, PrimaryKey, String, CHAR
 from MySQLAlchemy.exit.declarative import declarative_base()
+
 Base = declarative_base():
-class State(Base):
-    __tablename__ = "state"
-    name = Column(String(128))
-    id = Column(Integer, primary_key=True)
+    class State(Base):
+        """class definition"""
+        __tablename__ = "state"
+        name = Column(String(128))
+        id = Column(Integer, primary_key=True)
