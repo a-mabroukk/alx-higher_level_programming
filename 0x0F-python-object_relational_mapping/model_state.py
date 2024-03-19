d2650249c62580dbcd82d7e63c873e271763abc8
Alaa Fayed <alaafayed810@gmail.com>
2024-03-19 02:49:07 +0200

    6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """python file that contains the class definition of a
 State and an instance Base = declarative_base()"""
-from MySQLAlchemy import Column, Integer, PrimaryKey, String
-from MySQLAlchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
