add7e66d58e360f9510c2298c684dd30e1a93181
Alaa Fayed <alaafayed810@gmail.com>
2024-03-30 21:23:47 +0200

    3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep -i "Allow:" | awk "{print $2}"
+curl -s -I -X OPTIONS "$1" | grep -i "Allow:" | cut -f2- -d' '
