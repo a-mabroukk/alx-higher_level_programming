4658c3a748711f3cf512ba463276a7fd3cb54844
Alaa Fayed <alaafayed810@gmail.com>
2024-03-30 23:13:41 +0200

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script
-curl -s -L -X PUT -d "Origin: You got me!" -H  0.0.0.0:5000/catch_me
+curl -s -X POST -H "Content-Type: application/json" http://0.0.0.0:5000/catch_me >/dev/null
