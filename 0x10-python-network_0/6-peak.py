4cdf62b550b8897131ffe059dfaf615eaa1cd4ea
Alaa Fayed <alaafayed810@gmail.com>
2024-03-30 22:23:03 +0200

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Find a peak"""
+
+
 def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers"""
     i = 1
     if (not list_of_integers):
         return None
