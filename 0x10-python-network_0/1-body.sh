51f2e95bf0441da7a218424ca5c199c63cb82fbb
Alaa Fayed <alaafayed810@gmail.com>
2024-03-30 20:41:56 +0200

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sript that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -w "$1" "%{http_code}" -o /dev/null
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s -w "%{http_code}" -L "$1"
