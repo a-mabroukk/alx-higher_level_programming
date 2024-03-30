434e1d084c4067554ba2d8a054025cb717642c99
Alaa Fayed <alaafayed810@gmail.com>
2024-03-30 22:48:28 +0200

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,19 +3,10 @@
 
 
 def find_peak(list_of_integers):
-    """function that finds a peak in a list of unsorted integers"""
-    i = 1
-    if (not list_of_integers):
-        return None
-    if (len(list_of_integers) == 1):
-        return list_of_integers[0]
-    if (list_of_integers[0] >= list_of_integers[2]):
-        return list_of_integers[0]
-    if (len(list_of_integers) - 1 >= len(list_of_integers) - 2):
-        return len(list_of_integers) - 1
-    # check for every other element
-    for i in range(len(list_of_integers), len(list_of_integers) - 1):
-        if (list_of_integers[i] >= list_of_integers[i - 1] and
-                list_of_integers[i] >= list_of_integers[i + 1]):
-            return list_of_integers[i]
-    return find_peak(list_of_integers)
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
