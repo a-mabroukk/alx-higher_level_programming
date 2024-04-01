8ca5c47173a967eb9953886d13fc7fd038854959
Alaa Fayed <alaafayed810@gmail.com>
2024-04-02 00:18:18 +0200

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,10 +4,7 @@ displays the value of variable found in the header of the response"""
 from urllib import request
 from sys import argv
 
-url = argv[1]
 
 if __name__ == "__main__":
-    with request.urlopen(url) as response:
-        headers = response.info()
-        request_id = headers['X-Request-Id']
-        print(request_id)
+    with request.urlopen(argv[1]) as page:
+        print(page.getheader("X-Request-Id"))
