471dc12e5dd76fb5aafaad2846af2a88b5de5faf
Alaa Fayed <alaafayed810@gmail.com>
2024-04-02 00:15:44 +0200

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """importing modules to take in a URL, sends a request to the URL and
 displays the value of variable found in the header of the response"""
-import urllib.request
-import sys
+from urllib import request
+from sys import argv
 
-url = sys.argv[1]
+url = argv[1]
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         headers = response.info()
         request_id = headers['X-Request-Id']
         print(request_id)
